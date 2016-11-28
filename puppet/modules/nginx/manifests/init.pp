e1e0b38ef92b1e39f9feea7f07d47c5dddf8438c
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-11-28 22:21:18 +0100

    Adds nginx service

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -1,5 +1,15 @@
 class nginx {
+    package { 'apache2.2-common':
+        ensure => absent,
+    }
+    
     package { 'nginx':
         ensure => installed,
+        require => Package['apache2.2-common'],
+    }
+    
+    service { 'nginx':
+        ensure => running,
+        require => Package['nginx'],
     }
 }
