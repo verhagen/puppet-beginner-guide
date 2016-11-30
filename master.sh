85168da084baa8d5af1121bb668fb0d40717311a
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-11-30 22:55:06 +0100

    Adds hash sign so Puppet 3 is no longer the default

--- master.sh
+++ master.sh
@@ -5,8 +5,8 @@
 #dpkg -i puppetlabs-release-pc1-trusty.deb
 
 # Puppet 3
-wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-dpkg -i puppetlabs-release-trusty.deb
+#wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+#dpkg -i puppetlabs-release-trusty.deb
 
 apt-get update
 apt-get install -y puppetserver
